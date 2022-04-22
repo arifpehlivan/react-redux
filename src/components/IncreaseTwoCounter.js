86a110d69f12e08f012d4b42dae22cf0fbc6c58a
arifpehlivan <arifpehlivanapap@gmail.com>
2022-04-22 07:34:09 +0300

    counter redux

---
+++ src/components/IncreaseTwoCounter.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import {increaseTwoCounter} from "../redux/actions/counterActions"
+
+class IncreaseTwoCounter extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={e => {
+          this.props.dispatch(increaseTwoCounter());
+        }}>+2</button>
+      </div>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return {actions:bindActionCreators(increaseTwoCounter,dispatch)}
+}
+
+export default connect(mapDispatchToProps)(IncreaseTwoCounter);
--- src/redux/actions/counterActions.js
+++ src/redux/actions/counterActions.js
@@ -3,4 +3,14 @@ import * as actionTypes from "./actionTypes"
 export const increaseCounter = () => ({
     type:actionTypes.INCREASE_COUNTER,
     payload:1
-})
+});
+
+export const decreaseCounter = () => ({
+    type:actionTypes.DECREASE_COUNTER,
+    payload:1
+});
+
+export const increaseTwoCounter = () => ({
+    type:actionTypes.INCREASE_TWO_COUNTER,
+    payload:2
+});
